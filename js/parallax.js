f0d67082c490d6101d42d7b6bf89012388e5458f
petarpetrovt <petarpetrovt@gmail.com>
2014-10-19 16:31:41 +0300

    * Added scroll-arrows;

--- js/parallax.js
+++ js/parallax.js
@@ -11,7 +11,39 @@ $(document).ready(function() {
 			});
 	});
 
+    var up = $('#go-up');
+    var down = $('#go-down');
+
+    up.click(function () {
+       var hash = window.location.hash;
+       var currentNum = parseInt(hash.substr(8,2));
+
+        if(currentNum > 0)
+            scrollTo(currentNum - 1);
+    });
+
+    down.click(function () {
+        var hash = window.location.hash;
+        var currentNum = 0;
+
+        if(hash != "#slide-home")
+            currentNum = parseInt(hash.substr(8,2));
+
+        if(currentNum < 12)
+            scrollTo(currentNum + 1);
+    });
 });
+
+function scrollTo(num) {
+    var id = "#slide-p" + num;
+
+    if (num == 0)
+        id = "#slide-home";
+
+    $('html, body').animate({scrollTop: $(id).offset().top}, 1200, function () {
+        window.location = id;
+    });
+}
 
 function parallax() {
 	var scrollPosition = $(window).scrollTop();
