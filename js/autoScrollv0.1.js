511ca0022f924ae7e4e0002a0c2617650c75e361
petarpetrovt <petarpetrovt@gmail.com>
2014-10-22 10:31:07 +0300

    * Added generic method for menu buttons; * Minor refactoring;

--- js/autoScrollv0.1.js
+++ js/autoScrollv0.1.js
@@ -1,69 +1,75 @@
- $(document).ready(function() {
- 	window.location.hash = "slide-p1"
- 	var hash = window.location.hash;
- 	var num = hash.substr(8, 2);
- 	var count = $(".content").children().length;
- 	$("#page").val(num + " / " + count)
+$(document).ready(function () {
+    window.location.hash = "slide-p1"
+    var hash = window.location.hash;
+    var num = hash.substr(8, 2);
+    var count = $(".content").children().length;
+    $("#page").val(num + " / " + count)
 
- 	$("#go-up").click(function() {
- 		scrollToPrew();
- 	});
-
- 	$("#go-down").click(function() {
- 		scrollToNext();
- 	});
-
-    $('#logo').click(function(){
-        scroll("#slide-p1");
+    $("#go-up").click(function () {
+        scrollToPrew();
     });
 
- });
+    $("#go-down").click(function () {
+        scrollToNext();
+    });
 
- $(window).bind('mousewheel', function(event) {
- 	scrollToSlide(event);
- });
+    //Generic method for menuButtons
+    $('.MenuButton').click(function () {
+        var loc = window.location.toString();
+        var pageid = $(this).data("pageid");
 
- $(window).bind('touchmove', function(event) {
- 	scrollToSlide(event);
- });
+        if (loc.indexOf(pageid) > -1)
+            scroll("#slide-p1");
+        else
+            window.location = pageid;
+    });
+});
 
- function scrollToSlide(event) {
-	event.preventDefault();
- 	if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
- 		scrollToPrew();
- 	} else {
- 		scrollToNext();
- 	}
- }
+$(window).bind('mousewheel', function (event) {
+    scrollToSlide(event);
+});
 
- function scrollToNext() {
- 	var temp = window.location.hash.substr(1, 15),
+$(window).bind('touchmove', function (event) {
+    scrollToSlide(event);
+});
+
+function scrollToSlide(event) {
+    event.preventDefault();
+    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+        scrollToPrew();
+    } else {
+        scrollToNext();
+    }
+}
+
+function scrollToNext() {
+    var temp = window.location.hash.substr(1, 15),
  		next = "#" + $(".content").children("div[id='" + temp + "']").next().attr("id");
- 	scroll(next);
- };
+    scroll(next);
+};
 
- function scrollToPrew() {
- 	var temp = window.location.hash.substr(1, 15),
+function scrollToPrew() {
+    var temp = window.location.hash.substr(1, 15),
  		next = "#" + $(".content").children("div[id='" + temp + "']").prev().attr("id");
- 	scroll(next);
- };
+    scroll(next);
+};
 
- function scroll(hash) {
- 	//scroll & animation
-     if(hash == "#undefined")
+function scroll(hash) {
+    //scroll & animation
+    if (hash == "#undefined")
         return;
 
- 	$('html, body').stop().animate({
- 		'scrollTop': $(hash).offset().top - 50
- 	}, 700, 'swing', function() {
- 		window.location.hash = hash;
- 	});
- 	updatePageNum(hash);
- };
+    $('html, body').stop().animate({
+        'scrollTop': $(hash).offset().top - 50
+    }, 700, 'swing', function () {
+        window.location.hash = hash;
+    });
+    updatePageNum(hash);
+};
 
- function updatePageNum(hash) {
- 	//update page num
- 	var num = hash.substr(8, 2);
- 	var count = $(".content").children().length;
- 	$("#page").val(num + " / " + count);
- }
+function updatePageNum(hash) {
+    //update page num
+    var num = hash.substr(8, 2);
+    var count = $(".content").children().length;
+    $("#page").val(num + " / " + count);
+}
--- js/resizeHeight.js
+++ js/resizeHeight.js
@@ -1,20 +1,22 @@
 var nav = {};
 
-$(document).ready(function() {
-	var vph = $(window).height();
-	$('[id^="slide"]').css({
-		'height': vph + 'px'
-	});
-	$(window).resize(function() {
-		var vph = $(window).height();
-		$('[id^="slide"]').css({
-			'height': vph + 'px'
-		});
-	});
+$(document).ready(function () {
+    var vph = $(window).height();
+
+    $('[id^="slide"]').css({
+        'height': vph + 'px'
+    });
+
+    $(window).resize(function () {
+        var vph = $(window).height();
+        $('[id^="slide"]').css({
+            'height': vph + 'px'
+        });
+    });
 
     nav = $('header nav');
 
-      $('header nav a').click(function(){
-            nav.removeClass('in');
-       });
+    $('header nav a').click(function () {
+        nav.removeClass('in');
+    });
 });
